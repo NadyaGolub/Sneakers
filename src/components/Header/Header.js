5878d438b35fa95888ae9c6aec8f6ed41c6ec1c7
NadyaGolub <nadyagolub@ukr.net>
2023-08-18 23:07:50 +0300

    final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Basket, HeaderBox, HeaderLeft, HeaderRight, Img, NameShop, ShopTitle, SvgBasket } from "./Header.styled";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Basket,
+  HeaderBox,
+  HeaderLeft,
+  HeaderRight,
+  Img,
+  NameShop,
+  ShopTitle,
+  SvgBasket,
+} from './Header.styled';
 
-import AppContext from "../context";
-
+import AppContext from '../context';
 
 function Header(props) {
   const { cartItems } = React.useContext(AppContext);
-  
-  const totalPrice = cartItems.reduce((sum, obj) => Number(obj.price) + Number(sum), 0);
- 
-    return (
-      <HeaderBox>
-        <Link to="/">
+
+  const totalPrice = cartItems.reduce(
+    (sum, obj) => Number(obj.price) + Number(sum),
+    0
+  );
+
+  return (
+    <HeaderBox>
+      <Link to="/">
         <HeaderLeft>
-          
           <Img width={40} height={40} src="img/logo.png" alt="" />
           <div>
             <NameShop>React Sneakers</NameShop>
             <ShopTitle>Магазин кросівок</ShopTitle>
-            </div>
-            
-          </HeaderLeft>
-          </Link>
-        <HeaderRight>
-          <Basket onClick={props.onClickCart}>
-            <SvgBasket >
-              <use href="./img/symbol-defs.svg#basket"></use>
-            </SvgBasket>
-            <SvgBasket >
-              <use href="./img/symbol-defs.svg#heart-like"></use>
-            </SvgBasket>
-            <span>{ totalPrice} грн</span>
-          </Basket>
-          <li>
-            <Link to="/favorites">
+          </div>
+        </HeaderLeft>
+      </Link>
+      <HeaderRight>
+        <Basket onClick={props.onClickCart}>
+          <SvgBasket>
+            <use href="./img/symbol-defs.svg#basket"></use>
+          </SvgBasket>
+
+          <span>{totalPrice} грн</span>
+        </Basket>
+        <li>
+          <Link to="/favorites">
             <SvgBasket width={20} height={20}>
-              
-                <use href="./img/symbol-defs.svg#heart"></use>
-                
-              </SvgBasket>
-              </Link>
-          </li>
-          <li>
-            <Link to="/orders">
+              <use href="./img/symbol-defs.svg#heart"></use>
+            </SvgBasket>
+          </Link>
+        </li>
+        <li>
+          <Link to="/orders">
             <svg width={20} height={20}>
               <use href="./img/symbol-defs.svg#user"></use>
-              </svg>
-              </Link>
-          </li>
-        </HeaderRight>
-      </HeaderBox>
-    );
+            </svg>
+          </Link>
+        </li>
+      </HeaderRight>
+    </HeaderBox>
+  );
 }
 
 export default Header;
